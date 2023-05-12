af0100f416484aeeef4a862dc04fe9169b9acbac
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-05-12 13:38:46 -0300

    Update test_driver.py

--- srcprg/tests/test_driver.py
+++ srcprg/tests/test_driver.py
@@ -2,24 +2,24 @@
 #     assert new_driver.nome == 'Plautz'
 
 
-def test_new_driver_data_nasc(new_driver):
-    assert new_driver.data_nasc == '1958-10-01'
+# def test_new_driver_data_nasc(new_driver):
+#     assert new_driver.data_nasc == '1958-10-01'
 
 
-def test_new_driver_email(new_driver):
-    assert new_driver.email == 'plautz@email.com'
+# def test_new_driver_email(new_driver):
+#     assert new_driver.email == 'plautz@email.com'
 
 
-def test_new_driver_cpf(new_driver):
-    assert new_driver.cpf == '30172152968'
+# def test_new_driver_cpf(new_driver):
+#     assert new_driver.cpf == '30172152968'
 
 
-def test_new_driver_telefone(new_driver):
-    assert new_driver.telefone == '41996514346'
+# def test_new_driver_telefone(new_driver):
+#     assert new_driver.telefone == '41996514346'
 
 
-def test_new_driver_cnh(new_driver):
-    assert new_driver.cnh == '112345678901'
+# def test_new_driver_cnh(new_driver):
+#     assert new_driver.cnh == '112345678901'
 
 
 # def test_new_driver_endereco(new_driver):
